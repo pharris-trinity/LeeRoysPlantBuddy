d9a78b1580ec45aec46eebec1cdbadf0401efb57
scohen-trinity <scohen@trinity.edu>
2024-02-10 22:05:59 -0600

    added onto the controller method to dynamically render the products onto the product page using an in memory model and the mainController.getProducts function, also extrapolated out the fetching of the data to the getProducts function in the dataModel file, currently using an in memory model, will convert to a database soon.

--- models/dataModel.js
+++ models/dataModel.js
@@ -3,6 +3,16 @@ const dataModel = {
       // Logic to fetch data from a source
       return 'Hello, LeeRoy!';
     },
+    getProducts: () => {
+      let products = [
+        {id: 1, description: 'Cat Food'},
+        {id: 2, description: 'Pet Stuff'},
+        {id: 3, description: 'Dog Food'},
+        {id: 4, description: 'Bird Food'},
+        {id: 5, description: 'Lizard Food'},
+      ]
+      return products;
+    }
   };
   
   module.exports = dataModel;
