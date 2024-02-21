508af7bddd58ed7d4a1799a30bbdcce57266256f
scohen-trinity <scohen@trinity.edu>
2024-02-21 12:16:32 -0600

    rearranged the public and images folders so that images are in public now and now products can have image fields and be called more accurately otherwise placeholder code is there in case of a failure to load or no image

--- models/dataModel.js
+++ models/dataModel.js
@@ -1,5 +1,5 @@
 let products = [
-  {id: 1, description: 'Cat Food', price: 5.99},
+  {id: 1, description: 'Cat Food', price: 5.99, image: 'images/cat-food.jpg'},
   {id: 2, description: 'Pet Stuff', price: 5.99},
   {id: 3, description: 'Dog Food', price: 5.99},
   {id: 4, description: 'Bird Food', price: 5.99},
