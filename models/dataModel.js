37c0f52701baab30db2f95cc0cb0817d505a714c
scohen-trinity <scohen@trinity.edu>
2024-02-26 11:04:13 -0600

    made it so that the default for listed items is checked and the default for unlisted items is unchecked and fixed the ordering of what checking vs unchecking means

--- models/dataModel.js
+++ models/dataModel.js
@@ -1,7 +1,7 @@
 let products = [
   {id: 1, description: 'Cat Food', price: 5.99, image: 'images/cat-food.bmp', display: true},
   {id: 2, description: 'Dog Food', price: 5.99, image: 'images/dog-food.bmp', display: true},
-  {id: 3, description: 'Bird Food', price: 5.99, image: 'images/bird-food.bmp', display: true},
+  {id: 3, description: 'Bird Food', price: 5.99, image: 'images/bird-food.bmp', display: false},
   {id: 4, description: 'Lizard Food', price: 5.99, image: 'images/lizard-food.bmp', display: true},
 ]
 
