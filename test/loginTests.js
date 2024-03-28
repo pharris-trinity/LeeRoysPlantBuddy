b6e7f476e0ff8e820d53280387ef247ebe7e56b8
Colin Houston <chousto1@trinity.edu>
2024-03-28 14:01:22 -0500

    Made changes to testVerifyUser, loginTests.js still needs testing and work

--- test/loginTests.js
+++ test/loginTests.js
@@ -2,7 +2,7 @@ const {dataModel} = require('../models/dataModel');
 const userModel = require('../models/dataModel');
 
 const loginTests = {
-
+    //test functions are booleans, return true if tests passed and false if they fail
     testAddUser : (username, password) => {
         var mainUsers = userModel.users;
         var oldLength = users.length;
@@ -27,25 +27,24 @@ const loginTests = {
 
     testVerifyUser : (username, password) => {
         userVerified = dataModel.verifyUser(username, password);
-        var userType = userType[0]; //verifyUser returns {userType, id}
-        if (userType == 'failed') return false;
+        var userType = userVerified[0]; //verifyUser returns {userType, id}
+        var userId = userVerified[1];
 
         var mainUsers = userModel.users;
         var adminTemp = 0;
         var registeredTemp = 0;
         mainUsers.forEach(user => {
-            if(user.username == username && user.account_type == 'admin') {
+            if(user.username == username && user.account_type == 'admin' && user.userid == userId) {
                 adminTemp++;
             }
-            if(user.username == username && user.account_type == 'registered') {
+            if(user.username == username && user.account_type == 'registered' && user.userid == userId) {
                 registeredTemp++;
             }
         })
         var adminFailed = (adminTemp == 0 || adminTemp > 1);
         var registeredFailed = (registeredTemp == 0 || registeredTemp > 1);
         if (adminFailed || registeredFailed) return false;
-
-        
+        if (registeredTemp == 0 && adminTemp == 0 && userType != 'failed') return false;
 
         return true;
     }
