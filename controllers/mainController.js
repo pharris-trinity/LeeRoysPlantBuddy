54a67f66f4e2a62919c466a67b163b0f0afab847
Maci Flanagan <124389919+mcflanaga@users.noreply.github.com>
2024-04-05 14:39:46 -0500

    Update mainController.js

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,6 +1,5 @@
-const {dataModel, Users} = require('../models/dataModel');
-
-const users = new Users();
+const {dataModel} = require('../models/dataModel');
+const userModel = require('../models/dataModel');
 /* const users = []; */
 // const {dataModel} = require('../models/dataModel');
 
@@ -8,35 +7,52 @@ const users = new Users();
 
 const mainController = {
   getIndex: (req, res) => {
+    console.log("got home");
     const data = dataModel.getData();
     res.render('index', { data });
   },
   getHome: (req, res) => {
+    console.log("got home");
     res.render('home');
   },
-  getCheckout: (req, res) => {
-    const checkout = dataModel.getCheckout();
-    res.render('checkout', { checkout });
+  async getCheckout(req, res) {
+    // try {
+    //   console.log("checkout");
+    //   const cart = dataModel.getCheckout();
+    //   const user_id = req.cookies.user_id;
+    //   // res.redirect(200, '/checkout');
+    //   return res.status(200);
+    // } catch {
+    //   return res.status(500).json({ error: 'Internal Server Error' });
+    // }
+    const cart = dataModel.getCheckout();
+    const user_id = req.cookies.user_id;
+    res.render('checkout', { cart, user_id });
   },
   getLogin: (req, res) => {
+    console.log("get login");
     const login = dataModel.getLogin();
     res.render('login', { login });
   },
-  verifyUser: (req, res) => {
+  async verifyUser(req, res) {
     const username = req.body.username;
     const password = req.body.password;
-    console.log(`username: ${username} password: ${password}`);
-    const loggedIn = dataModel.verifyUser(username, password);
-    // console.log(loggedIn);
-    if(loggedIn==='admin') {
-      res.render('home-admin');
-    } else if(loggedIn==='registered') {
-      // console.log('go to registered home');
-      // figure out how to load a page from the controller, maybe response based?
-      res.render('index');
-    } else {
-      console.log('You trying to break in???');
+    const data = dataModel.verifyUser(username, password);
+    const user_id = data.id;
+    const verified = data.verified;
+    // console.log(user_id);
+    // console.log(verified);
+    
+    if(user_id !== -1) {
+      res.cookie('user_id', user_id, {
+        maxAge: 900000, httpOnly: false, SameSite: 'None',
+      })
+
+      return res.status(200).json({ verified });
     }
+    
+    // res.json(verified, user_id);
+    return res.status(401).json({error: 'Unauthorized'});
   },
   addUser: (req, res) => {
     const username = req.body.username;
@@ -50,26 +66,42 @@ const mainController = {
       console.log('Error: Invalid sign up!');
     }
   },
-  getProduct: (req, res) => {
-    const products = dataModel.getProducts();
-  
-    res.render('product', { products });
+  async getProduct(req, res) {
+    try {
+      const products = await userModel.getProducts();
+      const user_id = req.cookies.user_id;
+      // console.log(user_id);
+      res.render('product', { products, user_id });
+    } catch (error) {
+      console.error('Error fetching products:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
   },
-  getProductAdmin: (req, res) => { 
-    const products = dataModel.getProducts();
-
-    res.render('product-admin', {products});
+  async getProductAdmin(req, res) { 
+    try {
+      const products = await userModel.getProducts();
+      const user_id = req.cookies.user_id;
+      console.log(user_id);
+      res.render('product-admin', {products});
+    } catch (error) {
+      console.error('Error fetching products:', error);
+      res.status(500).json({ error: 'Internal Server Error'});
+    }
   },
   getLogin: (req, res) => {
   const login = dataModel.getLogin();
   res.render('login', { login });
   },
   addToCart: (req, res) => {
-    const id = parseInt(req.body.id);
+    const product_id = parseInt(req.body.id);
     const name = req.body.name;
     const price = parseFloat(req.body.price);
+    const cart_id = 1; // should always be the same as user_id as they have associated cart
+    // console.log(product_id);
+
+    userModel.addToCart(cart_id, product_id);
     
-    const carts = dataModel.addToCart(id, name, price);
+    const carts = dataModel.addToCart(product_id, name, price);
     // console.log(carts);
   },
   removeFromCart: (req, res) => {
@@ -100,13 +132,13 @@ const mainController = {
   },
   showToProducts: (req, res) => {
     const product_id = req.body.product_id;
-
-    dataModel.showToProducts(product_id);
+    userModel.showProduct(product_id);
+    // dataModel.showToProducts(product_id);
   },
   hideFromProducts: (req, res) => {
     const product_id = req.body.product_id;
-
-    dataModel.hideFromProducts(product_id);
+    userModel.hideProduct(product_id);
+    // dataModel.hideFromProducts(product_id);
   },
   addToProducts: (req, res) => {
     const name = req.body.name;
