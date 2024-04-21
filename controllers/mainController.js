5aab46a562fa67bfdc9de5e2591ad13d04d49514
scohen-trinity <scohen@trinity.edu>
2024-04-21 14:50:09 -0500

    cleaned up the codebase and ensured that everything is mostly working from an integration test standpoint, still have more that I want to do before we deploy

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -2,58 +2,47 @@ const {dataModel} = require('../models/dataModel');
 const userModel = require('../models/dataModel');
 
 const mainController = {
+  // Function to render the landing page
   getIndex: (req, res) => {
     const data = dataModel.getData();
     res.render('index', { data });
   },
+  // Function to render the home page
   getHome: (req, res) => {
     res.render('home');
   },
+  // Function to get items in a users cart and then display them at the checkout page
   async getCheckout(req, res) {
     const user_id = req.cookies.user_id;
     const cart = await userModel.getCart(user_id);
 
     res.render('checkout', { cart, user_id });
   },
-  getLogin: (req, res) => {
-    // const login = dataModel.getLogin();
-    res.render('login');
-  },
+  // Function that verifies that a user is in the db and if so issues a cookie and logs them in, otherwise returns an unauthorized error
   async verifyUser(req, res) {
     const username = req.body.username;
     const password = req.body.password;
-    console.log("username: ", username, "password: ", password);
     const result = await userModel.verifyUser(username, password);
-    console.log(result);
     const verified = result.acctype;
     const id = result.id;
-    console.log("this is verified stuff", verified);
-    console.log("this should be id", id);
     if(id !== -1) {
       res.cookie('user_id', id, {
         maxAge: 900000, httpOnly: false, SameSite: 'None',
-    })
+      })
 
-    return res.status(200).json({ verified });
+      return res.status(200).json({ verified });
     }
     
     return res.status(401).json({error: 'Unauthorized'});
   },
+  // Function that adds a user into the database
   async addUser(req, res) {
     const username = req.body.username;
     const password = req.body.password;
     const result = await userModel.addUser(username, password);
     return res.status(200);
-    //const newSignUp = userModel.addUser(username, password);
-    // if (result.account_type === 'admin') {
-    //   res.render('products-admin');
-    // } else if (result.account_type === 'registered') {
-    //   res.render('index');
-    // } else {
-    //   console.log('Error: Invalid sign up!');
-    // }
-
   },
+  // Function that fetches all products from the product database to display to a user
   async getProduct(req, res) {
     try {
       const products = await userModel.getProducts();
@@ -64,6 +53,7 @@ const mainController = {
       res.status(500).json({ error: 'Internal Server Error' });
     }
   },
+  // Function that lists all the admin products and logging trail for admin usage
   async getProductAdmin(req, res) { 
     try {
       const products = await userModel.getProducts();
@@ -76,68 +66,64 @@ const mainController = {
       res.status(500).json({ error: 'Internal Server Error'});
     }
   },
+  // Function that gets the login page
   getLogin: (req, res) => {
-  // const login = dataModel.getLogin();
-  res.render('login');
+    res.render('login');
   },
+  // Function to add an item to a cart if the add to cart button is clicked on the user product page
   async addToCart (req, res) {
     const product_id = parseInt(req.body.id);
     const name = req.body.name;
     const price = parseFloat(req.body.price);
     const cart_id = req.cookies.user_id; // should always be the same as user_id as they have associated cart
-    // console.log(product_id);
 
     userModel.addToCart(cart_id, product_id);
-    
-    // const carts = dataModel.addToCart(product_id, name, price);
-    // console.log(carts);
   },
+  // Function to decrement the number of items in a cart at the checkout page
   async removeFromCart(req, res) {
     try {
       const product = req.body.id;
       const user = req.cookies.user_id;
       const cart = await userModel.removeFromCart(user, product);
-      } catch (error) {
+    } catch (error) {
       console.error('Error removing from cart:', error);
       res.status(500).json({ error: 'Internal Server Error'});
-
     }
   },
+  // Function to increment the amount of a certain item at the checkout page
   async addQuantity(req, res) {
     try {
       const product = req.body.id;
       const user = req.cookies.user_id;
       const cart = await userModel.addQuantity(user, product);
-      // res.render('checkout', { cart, user });
-      } catch (error) {
+    } catch (error) {
       console.error('Error adding to cart:', error);
       res.status(500).json({ error: 'Internal Server Error'});
     }
   },
+  // Function to remove all items from a users cart when click buy now at the checkout page
   async emptyCart(req, res) {
     try {
       const id = req.cookies.user_id;
       const newcart = await userModel.emptyCart(id);
-      // console.log(newcart);
-      const cart = [];
-      // res.render('checkout', { cart, id });
     } catch (error) {
       console.error('Error checking out:', error);
       res.status(500).json({ error: 'Internal Server Error'});
     }
   },
+  // Function to show a product to users from the admin console
   showToProducts: (req, res) => {
     const product_id = req.body.product_id;
     const user_id = req.cookies.user_id;
     userModel.showProduct(user_id, product_id);
-    // dataModel.showToProducts(product_id);
   },
+  // Function to hide a product from users from the admin console
   hideFromProducts: (req, res) => {
     const product_id = req.body.product_id;
     const user_id = req.cookies.user_id;
     userModel.hideProduct(user_id, product_id);
-    // dataModel.hideFromProducts(product_id);
   },
+  // Function to add a new product to the database from the admin console
   addToProducts: (req, res) => {
     const name = req.body.name;
     const price = req.body.price;
--- models/dataModel.js
+++ models/dataModel.js
@@ -14,6 +14,62 @@ async function getUsers() {
   }
 }
 
+// Function to verify a user before loggin them in, checks if a user/password combination exists in the database for logging in
+async function verifyUser(username, password) {
+  const client = await pool.connect();
+  try {
+      const query = {
+        text: 'SELECT * FROM users WHERE username = $1 AND password = $2',
+        values: [username, password],
+      }
+      const result = await client.query(query);
+      var acctype = 'failed';
+      var id = -1;
+      if (result.rows.length == 1){
+        acctype = result.rows[0].account_type;
+        id = result.rows[0].user_id;
+      }
+      return {acctype, id};
+  } finally {
+    client.release();
+  }
+}
+
+async function addUser(username, password) {
+  const client = await pool.connect();
+  try {
+      console.log('made it to addUser function in dataModel');
+      const result = await client.query('SELECT COUNT(*) FROM carts');
+      const new_id = parseInt(result.rows[0].count) + 1;
+      console.log(new_id);
+      const acctype = 'registered';
+     // console.log(newid);
+     const cart_creation = {
+      text: "INSERT INTO carts (cart_id) VALUES ($1)",
+      values: [new_id]
+     }
+
+     client.query(cart_creation);
+
+      const query = {
+        text: "INSERT INTO users (username, password, account_type, cart_id) VALUES ($1, $2, $3, $4)",
+        values: [username, password, 'registered', new_id],
+      };
+
+      client.query(query);
+      var newUser = {userid: new_id, username: username, password: password, account_type: 'registered'};
+      return newUser;
+      
+      //  console.log(newUser);
+    //  console.log(users.length);
+     //users.push(newUser);
+     // console.log(users.length);
+     // console.log(users[users.length - 1]);
+  } finally {
+    client.release();
+  }
+}
+
 
 // Product Page Functions
 
@@ -100,6 +156,61 @@ async function emptyCart(cart_id) {
   }
 }
 
+async function removeFromCart(cart_id, product_id) {
+	const client = await pool.connect();
+	try {
+    const query = {
+      text: "SELECT * FROM cartitems WHERE cart_id = $1 AND product_id = $2",
+      values: [cart_id, product_id],
+    };
+    const result = await pool.query(query);
+    const value = result.rows[0].quantity;
+    if(value > 1) {
+      const query = {
+        text: "UPDATE cartitems SET quantity = quantity - 1 WHERE cart_id = $1 AND product_id = $2",
+        values: [cart_id, product_id],
+      };
+      const result = await client.query(query);
+      return result.rows;
+    } else {
+      const query = {
+        text: "DELETE FROM cartitems WHERE cart_id = $1 AND product_id = $2",
+        values: [cart_id, product_id],
+      };
+      const result = await client.query(query);
+      return result.rows;
+      }
+	} finally {
+		client.release();
+	}
+}
+
+async function addQuantity(cart_id, product_id) { 
+	const client = await pool.connect();
+	try {
+    const query = {
+      text: "UPDATE cartitems SET quantity = quantity + 1 WHERE cart_id = $1 AND product_id = $2 ORDER BY product_id",
+      values:[cart_id, product_id],
+    };
+    const result = await client.query(query);
+	} finally {
+		client.release();
+	}
+}
+async function emptyCart(id) {
+	const client = await pool.connect();
+	try {
+    const query = {
+      text: "DELETE FROM cartitems WHERE cart_id = $1",
+      values: [id],
+    };
+		const result = await client.query(query);
+		return result.rows;
+	} finally {
+		client.release();
+	}
+}
+
 
 // Admin Page Functions
 
@@ -183,172 +294,8 @@ async function getAdminActions() {
   }
 }
 
-async function removeFromCart(cart_id, product_id) {
-	const client = await pool.connect();
-	try {
-    const query = {
-      text: "SELECT * FROM cartitems WHERE cart_id = $1 AND product_id = $2",
-      values: [cart_id, product_id],
-    };
-    const result = await pool.query(query);
-    const value = result.rows[0].quantity;
-    if(value > 1) {
-      const query = {
-        text: "UPDATE cartitems SET quantity = quantity - 1 WHERE cart_id = $1 AND product_id = $2",
-        values: [cart_id, product_id],
-      };
-      const result = await client.query(query);
-      return result.rows;
-    } else {
-      const query = {
-        text: "DELETE FROM cartitems WHERE cart_id = $1 AND product_id = $2",
-        values: [cart_id, product_id],
-      };
-      const result = await client.query(query);
-      return result.rows;
-      }
-	} finally {
-		client.release();
-	}
-}
 
-async function addQuantity(cart_id, product_id) { 
-	const client = await pool.connect();
-	try {
-    const query = {
-      text: "UPDATE cartitems SET quantity = quantity + 1 WHERE cart_id = $1 AND product_id = $2 ORDER BY product_id",
-      values:[cart_id, product_id],
-    };
-    const result = await client.query(query);
-	} finally {
-		client.release();
-	}
-}
-async function emptyCart(id) {
-	const client = await pool.connect();
-	try {
-    const query = {
-      text: "DELETE FROM cartitems WHERE cart_id = $1",
-      values: [id],
-    };
-		const result = await client.query(query);
-		return result.rows;
-	} finally {
-		client.release();
-	}
-}
-
-/*
-old:
-
-    addUser: (username, password) => {
-      var lastUser = users[users.length - 1];
-      var newid = lastUser.userid + 1;
-      console.log(newid);
-      var newUser = {userid: newid, username: username, password: password, account_type: 'registered'};
-      console.log(newUser);
-      console.log(users.length);
-      users.push(newUser);
-      console.log(users.length);
-      console.log(users[users.length - 1]);
-      
-      return newUser;
-    },
-  */
-
-async function addUser(username, password) {
-  const client = await pool.connect();
-  try {
-      console.log('made it to addUser function in dataModel');
-      const result = await client.query('SELECT COUNT(*) FROM carts');
-      const new_id = parseInt(result.rows[0].count) + 1;
-      console.log(new_id);
-      const acctype = 'registered';
-     // console.log(newid);
-     const cart_creation = {
-      text: "INSERT INTO carts (cart_id) VALUES ($1)",
-      values: [new_id]
-     }
-
-     client.query(cart_creation);
-
-      const query = {
-        text: "INSERT INTO users (username, password, account_type, cart_id) VALUES ($1, $2, $3, $4)",
-        values: [username, password, 'registered', new_id],
-      };
-
-      client.query(query);
-      var newUser = {userid: new_id, username: username, password: password, account_type: 'registered'};
-      return newUser;
-      
-      //  console.log(newUser);
-    //  console.log(users.length);
-     //users.push(newUser);
-     // console.log(users.length);
-     // console.log(users[users.length - 1]);
-  } finally {
-    client.release();
-  }
-}
-
-    /*
-    scratch work:
-    async function verifyUser(username, password) {
-    const client = await pool.connect();
-    try {
-      const result = client.query('SELECT * FROM users WHERE username = user.username');
-      return (LOGIC TO CHECK USER TYPE (admin/registered))
-      } finally {
-        client.release();
-      }
-    }
-    }
-    */
-
-/*
-old:
-
-    verifyUser: (username, password) => {
-      var verified = 'failed';
-      var id = -1;
-      users.forEach(user => {
-        if(username==user.username&&password==user.password) {
-          if(user.account_type=='admin') {
-            verified = 'admin';
-          } else if(user.account_type=='registered') {
-            verified = 'registered';
-            id = user.userid;
-          } else {
-            user.account_type = 'registered';
-            verified = 'registered';
-          }
-        }
-      })
-*/
-
-// attempting to write an async function for verifyUser
-async function verifyUser(username, password) {
-  const client = await pool.connect();
-  try {
-      console.log('made it to async verifyUser function');
-      const query = {
-        text: 'SELECT * FROM users WHERE username = $1 AND password = $2',
-        values: [username, password],
-      }
-      const result = await client.query(query);
-      // console.log(result);
-      var acctype = 'failed';
-      var id = -1;
-      if (result.rows.length == 1){
-        acctype = result.rows[0].account_type;
-        id = result.rows[0].user_id;
-      }
-      return {acctype, id};
-  } finally {
-    client.release();
-  }
-}
-
+// Old in memory model, deprecated and scheduled for removal in a minor version change
 let users = [
   {userid: 1, username: 'pharris', password: 'password', account_type: 'registered'},
   {userid: 2, username: 'chouston', password: "drowssap", account_type: 'admin'},
@@ -375,69 +322,6 @@ const dataModel = {
       let cartProducts = dataModel.getProducts();
 	    return cartProducts;
     },
-    addUser: (username, password) => {
-      var lastUser = users[users.length - 1];
-      var newid = lastUser.userid + 1;
-      console.log(newid);
-      var newUser = {userid: newid, username: username, password: password, account_type: 'registered'};
-      console.log(newUser);
-      console.log(users.length);
-      users.push(newUser);
-      console.log(users.length);
-      console.log(users[users.length - 1]);
-      
-      return newUser;
-    },
-
-    /* sam's version, i think:
-    async function getCart(id) {
-    const client = await pool.connect();
-    try {
-      const result = client.query('SELECT * FROM cartitems WHERE cart_id = id');
-      return result.rows;
-      } finally {
-        client.release();
-      }
-    }
-    */
-
-    /*
-    trying:
-    async function verifyUser(username, password) {
-    const client = await pool.connect();
-    try {
-      const result = client.query('SELECT * FROM users WHERE username = user.username');
-      return (LOGIC TO CHECK USER TYPE (admin/registered))
-      } finally {
-        client.release();
-      }
-    }
-    }
-    */
-    //checks to make sure user is in the database and provides an id token
-    verifyUser: (username, password) => {
-      var verified = 'failed';
-      var id = -1;
-      users.forEach(user => {
-        if(username==user.username&&password==user.password) {
-          if(user.account_type=='admin') {
-            verified = 'admin';
-            id = user.userid;
-          } else if(user.account_type=='registered') {
-            verified = 'registered';
-            id = user.userid;
-          } else {
-            user.account_type = 'registered';
-            verified = 'registered';
-          }
-        }
-      })
-
-      // console.log(verified)
-      // console.log(verified);
-      // console.log(id);
-      return {verified: verified, id: id};
-    },
   };
 module.exports = 
 { dataModel, 
@@ -451,4 +335,7 @@ module.exports =
   getAdminActions, 
   emptyCart,
   verifyUser,
+  addUser,
+  removeFromCart,
+  addQuantity,
 };
