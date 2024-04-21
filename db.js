ad3617a067b23976e62d8d4468d083f545db2efd
scohen-trinity <scohen@trinity.edu>
2024-04-21 14:50:55 -0500

    made an important comment in db.js

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
     database: 'leeroyspets',
-    password: '',
+    password: '', // this needs to be clear whenever you push to a branch for security reasons
     port: 5432,
 });
 
