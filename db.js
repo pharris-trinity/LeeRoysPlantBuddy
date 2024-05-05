24e7586807a528e0c1fa7122c85a1aa90cd774c1
Trevor Skeen <tskeen1@trinity.edu>
2024-05-05 11:31:07 -0500

    fixed negative fields and non empty fields

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
     database: 'leeroyspets',
-    password: '', // this needs to be clear whenever you push to a branch for security reasons
+    password: '181134', // this needs to be clear whenever you push to a branch for security reasons
     port: 5432,
 });
 
